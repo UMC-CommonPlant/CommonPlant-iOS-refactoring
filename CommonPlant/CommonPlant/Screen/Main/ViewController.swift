582f22ca42c907dd22aca7b931b25ff808ac32ba
Yaewon Lee <yw990324@gmail.com>
2023-07-04 15:58:14 +0900

    [Feat] 프로젝트 초기 세팅

---
+++ CommonPlant/CommonPlant/Screen/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonPlant
+//
+//  Created by 이예원 on 2023/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
