7a4e1969adae3ca193e9cfcfb1618261a7d7edd9
Yaewon Lee <yw990324@gmail.com>
2023-07-06 15:35:38 +0900

    [CHORE] UIColor+Extension 생성

--- CommonPlant/CommonPlant/Global/Extensions/UIColor+Extension.swift
+++ CommonPlant/CommonPlant/Global/Extensions/UIColor+Extension.swift
@@ -5,4 +5,25 @@
 //  Created by 이예원 on 2023/07/06.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    // Primary Color
+    class var seaGreen: UIColor? { return UIColor(named: "SeaGreen") }
+    class var seaGreenDark1: UIColor? { return UIColor(named: "SeaGreenDark1") }
+    class var seaGreenDark2: UIColor? { return UIColor(named: "SeaGreenDark2") }
+    class var seaGreenDark3: UIColor? { return UIColor(named: "SeaGreenDark3") }
+    class var seaGreenLight: UIColor? { return UIColor(named: "SeaGreenLight") }
+    
+    // Gray Scale
+    class var gray1: UIColor? { return UIColor(named: "Gray1") }
+    class var gray2: UIColor? { return UIColor(named: "Gray2") }
+    class var gray3: UIColor? { return UIColor(named: "Gray3") }
+    class var gray4: UIColor? { return UIColor(named: "Gray4") }
+    class var gray5: UIColor? { return UIColor(named: "Gray5") }
+    class var gray6: UIColor? { return UIColor(named: "Gray6") }
+    
+    // Active Color
+    class var activeBlue: UIColor? { return UIColor(named: "ActiveBlue") }
+    class var activeRed: UIColor? { return UIColor(named: "ActiveRed") }
+}
