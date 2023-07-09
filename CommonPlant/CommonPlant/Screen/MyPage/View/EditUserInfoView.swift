0c0dfefd3132589be8f18d862b5d8f949a8b225a
orzr <tjsdkfk77@naver.com>
2023-07-09 11:17:33 +0900

    [FEAT] 회원 프로필 이미지 분리 및 EditUserInfoView 파일 생성 #3

---
+++ CommonPlant/CommonPlant/Screen/MyPage/View/EditUserInfoView.swift
@@ -0,0 +1,40 @@
+//
+//  EditUserInfoView.swift
+//  CommonPlant
+//
+//  Created by 아라 on 2023/07/09.
+//
+
+import UIKit
+
+class EditUserInfoView: UIView {
+    // MARK: Properties
+    
+    // MARK: UI Components
+    
+    // MARK: Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Custom Method
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+}
