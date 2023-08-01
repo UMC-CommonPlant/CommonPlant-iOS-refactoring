34ab2606fec9b9d3e58a7d4ec6cf36224562fc8f
Yaewon Lee <yw990324@gmail.com>
2023-08-01 16:01:54 +0900

    [FEAT] 인기검색어 테이블뷰 모델 생성

---
+++ CommonPlant/CommonPlant/Screen/PlantSearch/Model/FamousSearchModel.swift
@@ -0,0 +1,16 @@
+//
+//  FamousSearchModel.swift
+//  CommonPlant
+//
+//  Created by 이예원 on 2023/08/01.
+//
+
+import Foundation
+
+struct FamousSearchModel {
+    var plantImage: String
+    var plantName: String
+    var scientificName: String
+    var searchCount: String
+    var searchDate: String
+}
