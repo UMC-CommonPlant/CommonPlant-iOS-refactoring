ab2494fddfcf5448d4c56f7efd6ba8317e15aca8
Yaewon Lee <yw990324@gmail.com>
2023-08-07 18:38:25 +0900

    [FEAT] 셀 테두리에 그림자 추가 및 테이블뷰에서 컬렉션뷰로 변경

--- CommonPlant/CommonPlant/Screen/PlantSearch/ViewController/PopularSearchTableViewCell.swift
+++ CommonPlant/CommonPlant/Screen/PlantSearch/ViewController/PopularSearchTableViewCell.swift
@@ -8,22 +8,22 @@
 import UIKit
 import SnapKit
 
-class PopularSearchTableViewCell: UITableViewCell {
-    static let identifier = "PopularSearchTableViewCell"
+class PopularSearchCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PopularSearchCollectionViewCell"
     let plantImage = UIImageView()
     let nameLabel = UILabel()
     let scientificNameLabel = UILabel()
     let searchCountLabel = UILabel()
-    let mainBackView = UIView()
-    let shadowView = UIView()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         configureCell()
+        setupCorner()
+
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(\(coder) has not been implemented")
     }
     
     private func configureCell() {
@@ -31,34 +31,15 @@ class PopularSearchTableViewCell: UITableViewCell {
         setConstraints()
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        setupCorner()
-        mainBackView.layer.masksToBounds = true
-        let radius = shadowView.layer.bounds
-        shadowView.layer.shadowPath = UIBezierPath(roundedRect: shadowView.layer.bounds, cornerRadius: 16).cgPath
-        
-    }
-    
     // MARK: - UI
     private func setupCorner() {
-        mainBackView.layer.cornerRadius = 16
-      //  mainBackView.layer.masksToBounds = false
-      //  mainBackView.clipsToBounds = false
-        self.contentView.clipsToBounds = false
-        self.mainBackView.backgroundColor = .white
-        
-        self.layer.masksToBounds = false
-        self.clipsToBounds = false
-        self.layer.cornerRadius = 16
+        self.backgroundColor = .white
         self.layer.shadowColor = UIColor(red: 0.471, green: 0.471, blue: 0.471, alpha: 0.25).cgColor
         self.layer.shadowOffset = CGSize(width: 0, height: 1)
         self.layer.shadowOpacity = 1
         self.layer.shadowRadius = 4
-        
+        self.layer.cornerRadius = 16
     }
-    
-    
     
     func setAttributes(with model: PoplularSearchModel) {
         plantImage.layer.cornerRadius = 16
@@ -78,22 +59,24 @@ class PopularSearchTableViewCell: UITableViewCell {
     }
     
     private func setConstraints() {
-        self.contentView.addSubview(shadowView)
-        self.contentView.addSubview(mainBackView)
-        
         [plantImage, nameLabel, scientificNameLabel, searchCountLabel].forEach {
-            mainBackView.addSubview($0)
+            contentView.addSubview($0)
         }
+//        
+//        self.contentView.snp.makeConstraints {
+//            $0.width.equalTo(335)
+//            $0.left.right.equalToSuperview().offset(0)
+//        }
         
         plantImage.snp.makeConstraints {
             $0.width.equalTo(114)
             $0.height.equalTo(88)
-            $0.left.top.equalToSuperview().offset(10)
+            $0.left.top.equalTo(contentView).offset(10)
         }
         
         nameLabel.snp.makeConstraints {
             $0.height.equalTo(22)
-            $0.top.equalToSuperview().offset(18)
+            $0.top.equalTo(contentView).offset(18)
             $0.left.equalTo(plantImage.snp.right).offset(8)
         }
         
@@ -105,18 +88,8 @@ class PopularSearchTableViewCell: UITableViewCell {
         
         searchCountLabel.snp.makeConstraints {
             $0.height.equalTo(16)
-            $0.right.equalToSuperview().offset(-12)
-            $0.top.equalToSuperview().offset(78)
-        }
-        
-        mainBackView.snp.makeConstraints {
-            $0.left.right.top.equalToSuperview()
-            $0.height.equalTo(108)
-        }
-        
-        shadowView.snp.makeConstraints {
-            $0.left.right.top.equalToSuperview()
-            $0.height.equalTo(108)
+            $0.right.equalTo(contentView).offset(-12)
+            $0.top.equalTo(contentView).offset(78)
         }
     }
 }
