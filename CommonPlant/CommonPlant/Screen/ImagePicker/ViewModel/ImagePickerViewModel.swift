16592435b12f40c75c1c2c7ff1c8446cc9687be0
SunAra25 <tjsdkfk77@naver.com>
2023-08-19 16:05:01 +0900

    [FEAT] 이미지 load 로직 추가 #16

--- CommonPlant/CommonPlant/Screen/ImagePicker/ViewModel/ImagePickerViewModel.swift
+++ CommonPlant/CommonPlant/Screen/ImagePicker/ViewModel/ImagePickerViewModel.swift
@@ -1,3 +1,10 @@
+//
+//  ImagePickerViewModel.swift
+//  CommonPlant
+//
+//  Created by 아라 on 2023/08/18.
+//
+
 import Foundation
 import RxSwift
 import RxCocoa
@@ -15,5 +22,48 @@ class ImagePickerViewModel {
         let screenWidth = UIScreen.main.bounds.width
         let length = (screenWidth - (cellItemForRow - 1) * minimumSpacing) / cellItemForRow
         return CGSize(width: length, height: length)
+    }
+    
+    func loadImages(completion: @escaping () -> Void) {
+        self.accessImageURLs = BehaviorSubject(value: [])
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+
+        let fetchGroup = DispatchGroup()
+        
+        fetchResult.enumerateObjects { (asset, _, _) in
+            fetchGroup.enter()
+            self.getURL(ofPhotoWith: asset) { imageUrl in
+                if let imageUrl = imageUrl {
+                    do {
+                        var tempURLs = try self.accessImageURLs.value()
+                        tempURLs.append(imageUrl)
+                        self.accessImageURLs.onNext(tempURLs)
+                        
+                        fetchGroup.leave()
+                    } catch {
+                        print("Error getting the value of accessImageURLs")
+                    }
+                }
+            }
+        }
+        
+        fetchGroup.notify(queue: .main) {
+            completion()
+        }
+    }
+    
+    func getURL(ofPhotoWith mPhasset: PHAsset, completionHandler : @escaping ((_ responseURL : URL?) -> Void)) {
+        let options = PHContentEditingInputRequestOptions()
+        options.canHandleAdjustmentData = { _ in return true }
+        mPhasset.requestContentEditingInput(with: options, completionHandler: { input, info in
+            if let url = input?.fullSizeImageURL {
+                completionHandler(url)
+            } else {
+                completionHandler(nil)
+            }
+        })
     }
 }
