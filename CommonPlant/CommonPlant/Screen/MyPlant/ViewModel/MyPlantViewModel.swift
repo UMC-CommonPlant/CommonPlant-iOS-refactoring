3ff3f72e102806fc0f443f6416ac3e01d46ec08f
SunAra25 <tjsdkfk77@naver.com>
2023-08-25 00:17:17 +0900

    [FEAT] MyPlantViewModel 생성 #21

---
+++ CommonPlant/CommonPlant/Screen/MyPlant/ViewModel/MyPlantViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  MyPlantViewModel.swift
+//  CommonPlant
+//
+//  Created by 아라 on 2023/08/24.
+//
+
+import Foundation
+import RxSwift
+
+class MyPlantViewModel {
+    var plant: BehaviorSubject<MyPlant>
+    
+    init() {
+        let myPlant = MyPlant(
+            nickname: "몬테", scientificName: "Monstera deliciosa", place: "스윗홈_거실",
+            imgURL: "https://firebasestorage.googleapis.com/v0/b/common-plant.appspot.com/o/commonPlant_plant/몬테_fgbdLj?alt=media",
+            countDate: 1, remainderDate: -3, createdAt: "2022.11.24", wateredDate: "2022.11.24", waterDay: 10, sunlight: "밝은곳을 좋아해요!", tempMin: 16, tempMax: 20, humidity: "70% 이상",
+            memoList: [
+                Memo(userNickName: "커먼플랜트", userImgURL: "https://firebasestorage.googleapis.com/v0/b/common-plant.appspot.com/o/01eda71b-0e68-1acd-8e39-b74e1fa53847?alt=media", content: "장마여서 물주는 날짜를 조금 늦춤 하지만 해는 맑구나 몬테랑 함께...", imgURL: "https://firebasestorage.googleapis.com/v0/b/common-plant.appspot.com/o/commonPlant_plant/몬테_fgbdLj?alt=media", createdAt: "2022.11.20")
+            ])
+        
+        plant = BehaviorSubject(value: myPlant)
+    }
+}
