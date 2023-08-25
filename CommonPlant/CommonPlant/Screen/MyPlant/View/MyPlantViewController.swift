a0f6d3414b8a3abc8d4ed67e6ce4adc3590989bb
SunAra25 <tjsdkfk77@naver.com>
2023-08-26 00:53:24 +0900

    [FEAT] MyPlantView attribute 설정 #21

--- CommonPlant/CommonPlant/Screen/MyPlant/View/MyPlantViewController.swift
+++ CommonPlant/CommonPlant/Screen/MyPlant/View/MyPlantViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import RxSwift
 class MyPlantViewController: UIViewController {
     // MARK: Properties
     var countDate: Int = 0
@@ -14,17 +14,23 @@ class MyPlantViewController: UIViewController {
     var minTemperature: Int = 0
     var maxTemperature: Int = 0
     
+    let viewModel = MyPlantViewModel()
+    let disposeBag = DisposeBag()
+    
     // MARK: UIComponents
     var scrollView = UIScrollView()
     var stackView = UIStackView()
     var backButton = UIButton()
     var plantProfileView = UIView()
+    var menuButton = UIButton()
     var plantImageView = UIImageView()
+    var placeView = UIView()
+    var placeImageView = UIImageView()
     var placeNameLabel = UILabel()
     var nickNameLabel = UILabel()
     var scientificNameLabel = UILabel()
     var dateInfoView = UIView()
-    var countingMessageLabe = UILabel()
+    var countingMessageLabel = UILabel()
     var wateringImageView = UIImageView()
     var waterDayLabel = UILabel()
     var metMessageLabel = UILabel()
@@ -32,10 +38,11 @@ class MyPlantViewController: UIViewController {
     var lastWateringMessageLabel = UILabel()
     var lastWateringDateLabel = UILabel()
     var memoView = UIView()
-    var memoTitleLabel = UILabel()
+    var memoTitleLabel = CommonLabel()
+    var nextButton = UIButton()
     lazy var memoCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.scrollDirection = .horizontal
+        flowLayout.scrollDirection = .vertical
         flowLayout.minimumLineSpacing = 0
         flowLayout.minimumInteritemSpacing = 8
         flowLayout.itemSize = CGSize(width: self.view.bounds.width, height: 174)
@@ -45,19 +52,141 @@ class MyPlantViewController: UIViewController {
     }()
     var addMemoButton = UIButton()
     var plantInfoView = UIView()
+    var infoTitleLabel = CommonLabel()
     var backgroundView = UIView()
     var wateringCycleImage = UIImageView()
     var wateringCycleLabel = UILabel()
-    var sunlightImage = UIImage()
+    var cautionLabel = UILabel()
+    var sunlightImageView = UIImageView()
     var sunlightInfoLabel = UILabel()
-    var temperInfoImage = UIImage()
-    var temperInfoLabel = UILabel()
-    var humidityInfoImage = UIImage()
+    var temperatureImageView = UIImageView()
+    var temperatureLabel = UILabel()
+    var humidityInfoImageView = UIImageView()
     var humidityInfoLabel = UILabel()
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .gray1
+        self.navigationItem.title = "My Plant"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.bodyM1, .foregroundColor: UIColor.gray6 as Any]
+    }
+    
+    // MARK: Custom Methods
+    func setAttributes() {
+        let plantData = viewModel.myPlant
+        var nextBtnConfig = UIButton.Configuration.plain()
+        var addBtnConfig = UIButton.Configuration.plain()
         
+        plantImageView.load(url: URL(string: plantData.imgURL)!)
+        
+        placeView.backgroundColor = .seaGreenDark3
+        
+        placeImageView.image = UIImage(named: "Place")
+        
+        placeNameLabel.text = "\(plantData.place)"
+        placeNameLabel.textColor = .white
+        placeNameLabel.font = .bodyB3
+        
+        nickNameLabel.text = "\(plantData.nickname)"
+        nickNameLabel.textColor = .black
+        nickNameLabel.font = .head5
+        nickNameLabel.textAlignment = .center
+        
+        scientificNameLabel.text = "\(plantData.scientificName)"
+        scientificNameLabel.textColor = .gray6
+        scientificNameLabel.font = .bodyM2
+        scientificNameLabel.textAlignment = .center
+        
+        countingMessageLabel.text = "\(plantData.nickname)와(과) 함께한지 \(plantData.countDate)일이 지났어요!"
+        countingMessageLabel.partiallyChanged(targetString: "\(plantData.countDate)일", font: .bodyB1, color: .gray6)
+        countingMessageLabel.textColor = .gray5
+        countingMessageLabel.font = .bodyM2
+        countingMessageLabel.textAlignment = .center
+        
+        wateringImageView.image = UIImage(named: "WateringCan")
+        
+        waterDayLabel.text = "D\(plantData.remainderDate)"
+        waterDayLabel.textColor = .gray6
+        waterDayLabel.font = .head4
+        waterDayLabel.textAlignment = .center
+        
+        metMessageLabel.text = "처음 함께한 날"
+        metMessageLabel.textColor = .gray4
+        metMessageLabel.font = .captionM1
+        metMessageLabel.textAlignment = .center
+        
+        metDateLabel.text = plantData.createdAt
+        metDateLabel.textColor = .gray4
+        metDateLabel.font = .captionM1
+        metDateLabel.textAlignment = .center
+        
+        lastWateringMessageLabel.text = "마지막으로 물 준 날짜"
+        lastWateringMessageLabel.textColor = .gray4
+        lastWateringMessageLabel.font = .captionM1
+        lastWateringMessageLabel.textAlignment = .center
+        
+        lastWateringDateLabel.text = plantData.wateredDate
+        lastWateringDateLabel.textColor = .gray4
+        lastWateringDateLabel.font = .captionM1
+        lastWateringDateLabel.textAlignment = .center
+        
+        memoTitleLabel.text = "Memo"
+        memoTitleLabel.textColor = .gray4
+        memoTitleLabel.font = .bodyB1
+        memoTitleLabel.padding = UIEdgeInsets(top: 12, left: 10, bottom: 12, right: 0)
+        
+        nextBtnConfig.image = UIImage(named: "Next")
+        nextButton.configuration = nextBtnConfig
+        
+        var addMemoAttr = AttributedString.init("작성하기")
+        addMemoAttr.font = .bodyB3
+        addMemoAttr.foregroundColor = .white
+        addBtnConfig.attributedTitle = addMemoAttr
+        
+        addMemoButton.configuration = addBtnConfig
+        addMemoButton.contentHorizontalAlignment = .center
+        addMemoButton.backgroundColor = .seaGreenDark1
+        addMemoButton.makeRound(radius: 8)
+        
+        infoTitleLabel.text = "식물정보"
+        infoTitleLabel.textColor = .gray4
+        infoTitleLabel.font = .bodyB1
+        infoTitleLabel.padding = UIEdgeInsets(top: 12, left: 10, bottom: 12, right: 0)
+        
+        backgroundView.backgroundColor = .seaGreen
+        
+        wateringCycleImage.image = UIImage(named: "WateringPot")
+        
+        wateringCycleLabel.text = "\(plantData.waterDay) Day"
+        wateringCycleLabel.textColor = .gray6
+        wateringCycleLabel.font = .captionM1
+        wateringCycleLabel.textAlignment = .left
+        
+        cautionLabel.text = "물을 좋아하나 과습에 주의하세요!" // 현재 api에 없는 값!
+        cautionLabel.textColor = .gray5
+        cautionLabel.font = .captionM2
+        cautionLabel.textAlignment = .center
+        
+        sunlightImageView.image = UIImage(named: "Sunlight")
+        
+        sunlightInfoLabel.text = "\(plantData.sunlight)"
+        sunlightInfoLabel.textColor = .gray6
+        sunlightInfoLabel.font = .captionM1
+        sunlightInfoLabel.textAlignment = .left
+        
+        temperatureImageView.image = UIImage(named: "Temperature")
+        
+        temperatureLabel.text = "\(plantData.tempMin)~\(plantData.tempMax)℃"
+        temperatureLabel.textColor = .gray6
+        temperatureLabel.font = .captionM1
+        temperatureLabel.textAlignment = .left
+        
+        humidityInfoImageView.image = UIImage(named: "Humidity")
+        
+        humidityInfoLabel.text = "\(plantData.humidity)"
+        humidityInfoLabel.textColor = .gray6
+        humidityInfoLabel.font = .captionM1
+        humidityInfoLabel.textAlignment = .left
     }
 }
--- CommonPlant/CommonPlant/Screen/MyPlant/ViewModel/MyPlantViewModel.swift
+++ CommonPlant/CommonPlant/Screen/MyPlant/ViewModel/MyPlantViewModel.swift
@@ -9,10 +9,10 @@ import Foundation
 import RxSwift
 
 class MyPlantViewModel {
-    var plant: BehaviorSubject<MyPlant>
+    var myPlant: MyPlant
     
     init() {
-        let myPlant = MyPlant(
+        let plant = MyPlant(
             nickname: "몬테", scientificName: "Monstera deliciosa", place: "스윗홈_거실",
             imgURL: "https://firebasestorage.googleapis.com/v0/b/common-plant.appspot.com/o/commonPlant_plant/몬테_fgbdLj?alt=media",
             countDate: 1, remainderDate: -3, createdAt: "2022.11.24", wateredDate: "2022.11.24", waterDay: 10, sunlight: "밝은곳을 좋아해요!", tempMin: 16, tempMax: 20, humidity: "70% 이상",
@@ -20,6 +20,6 @@ class MyPlantViewModel {
                 Memo(userNickName: "커먼플랜트", userImgURL: "https://firebasestorage.googleapis.com/v0/b/common-plant.appspot.com/o/01eda71b-0e68-1acd-8e39-b74e1fa53847?alt=media", content: "장마여서 물주는 날짜를 조금 늦춤 하지만 해는 맑구나 몬테랑 함께...", imgURL: "https://firebasestorage.googleapis.com/v0/b/common-plant.appspot.com/o/commonPlant_plant/몬테_fgbdLj?alt=media", createdAt: "2022.11.20")
             ])
         
-        plant = BehaviorSubject(value: myPlant)
+        myPlant = plant
     }
 }
