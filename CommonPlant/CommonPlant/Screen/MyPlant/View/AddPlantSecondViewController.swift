cc0f2d95cd9450e7a6e3ee759eaf7466180de1ad
SunAra25 <tjsdkfk77@naver.com>
2024-02-04 22:58:46 +0900

    [FEAT] 식물 등록(2/2) 제외한 제약 조건 메서드 구현 및 설정 수정 #26

--- CommonPlant/CommonPlant/Screen/MyPlant/View/AddPlantSecondViewController.swift
+++ CommonPlant/CommonPlant/Screen/MyPlant/View/AddPlantSecondViewController.swift
@@ -31,7 +31,7 @@ class AddPlantSecondViewController: UIViewController {
     }()
     private let nameBackgroundView: UIView = {
         let view = UIView()
-        view.backgroundColor = .seaGreenLight
+        view.backgroundColor = .seaGreen
         return view
     }()
     private let nameLabel: UILabel = {
@@ -117,6 +117,7 @@ class AddPlantSecondViewController: UIViewController {
         flowLayout.itemSize = CGSize(width: 250, height: 156)
         flowLayout.sectionInset = UIEdgeInsets.init(top: 0, left: 20, bottom: 0, right: 20)
         let view = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        view.isHidden = true
         return view
     }()
     private let dateView: UIView = {
@@ -146,6 +147,7 @@ class AddPlantSecondViewController: UIViewController {
     }()
     private let calendarView: UIView = {
         let view = UIView()
+        view.isHidden = true
         return view
     }()
     private let selectedMonthLabel: UILabel = {
@@ -200,7 +202,7 @@ class AddPlantSecondViewController: UIViewController {
         let label = UILabel()
         label.text = "선택하지 않을 시, 등록일을 기준으로 설정합니다"
         label.font = .captionM2
-        label.textColor = .seaGreen
+        label.textColor = .seaGreenDark3
         return label
     }()
     private let cancleButton: UIButton = {
@@ -210,9 +212,9 @@ class AddPlantSecondViewController: UIViewController {
         attribute.font = .bodyM3
         config.attributedTitle = attribute
         config.baseForegroundColor = .white
-        config.background.cornerRadius = 4
         button.configuration = config
         button.backgroundColor = .gray5
+        button.makeRound(radius: 4)
         return button
     }()
     private let submitButton: UIButton = {
@@ -222,21 +224,24 @@ class AddPlantSecondViewController: UIViewController {
         attribute.font = .bodyM3
         config.attributedTitle = attribute
         config.baseForegroundColor = .white
-        config.background.cornerRadius = 4
         button.configuration = config
-        button.backgroundColor = .seaGreen
+        button.backgroundColor = .seaGreenDark1
+        button.makeRound(radius: 4)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setHierarchy()
+        setConstraints()
     }
     
     func setHierarchy() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         
-        [plantView, nameBackgroundView, nicknameView, placeBackgroundView, placeCollectionView, dateView, calendarView, cancleButton, submitButton].forEach {
+        [plantView, nameBackgroundView, nicknameView, placeBackgroundView, placeCollectionView, dateView, calendarView, messageLabel, cancleButton, submitButton].forEach {
             contentView.addSubview($0)
         }
         
@@ -256,8 +261,190 @@ class AddPlantSecondViewController: UIViewController {
             dateView.addSubview($0)
         }
         
-        [selectedMonthLabel, previousButton, nextButton, weekStackView, datePickerCollectionView, messageLabel].forEach {
+        [selectedMonthLabel, previousButton, nextButton, weekStackView, datePickerCollectionView].forEach {
             calendarView.addSubview($0)
+        }
+    }
+    
+    func setConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(scrollView.snp.width)
+            make.height.greaterThanOrEqualToSuperview().priority(.low)
+        }
+        
+        plantView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(24)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(120)
+        }
+        
+        plantImageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.width.height.equalTo(100)
+        }
+        
+        cameraImageView.snp.makeConstraints { make in
+            make.trailing.bottom.equalToSuperview()
+            make.width.height.equalTo(40)
+        }
+        
+        nameBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(plantView.snp.bottom).offset(32)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(56)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        nameUnderlineView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        nicknameView.snp.makeConstraints { make in
+            make.top.equalTo(nameBackgroundView.snp.bottom).offset(32)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(56)
+        }
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        nicknameCountLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        nicknameUnderlineView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        placeBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(nicknameView.snp.bottom).offset(32)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(56)
+        }
+        
+        placeChoiceLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.width.height.equalTo(24)
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(12)
+            make.width.height.equalTo(40)
+        }
+        
+        selectedPlaceLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalTo(deleteButton.snp.leading)
+        }
+        
+        placeUnderlineView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        placeCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(placeBackgroundView.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        dateView.snp.makeConstraints { make in
+            make.top.equalTo(placeCollectionView.isHidden ? placeBackgroundView.snp.bottom : placeCollectionView.snp.bottom).offset(32)
+            make.leading.trailing.equalToSuperview() // TODO: safeArea에 맞춘거랑 비교해보기
+            make.height.equalTo(56)
+        }
+        
+        lastWateredDayLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        selectedDateLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        dateUnderlineView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        calendarView.snp.makeConstraints { make in
+            make.top.equalTo(dateView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        selectedMonthLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.equalToSuperview().offset(12)
+            make.height.equalTo(24)
+        }
+        
+        previousButton.snp.makeConstraints { make in
+            make.centerY.equalTo(selectedMonthLabel.snp.centerY)
+            make.trailing.equalTo(nextButton.snp.leading).offset(-6)
+            make.width.height.equalTo(36)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.centerY.equalTo(selectedMonthLabel.snp.centerY)
+            make.trailing.equalToSuperview()
+            make.width.height.equalTo(36)
+        }
+        
+        weekStackView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(selectedMonthLabel.snp.bottom).offset(4)
+            make.height.equalTo(40)
+            make.bottom.equalToSuperview()
+        }
+        
+        datePickerCollectionView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(weekStackView.snp.bottom)
+            make.bottom.equalToSuperview()
+        }
+        
+        messageLabel.snp.makeConstraints { make in
+            make.top.equalTo(calendarView.isHidden ? dateView.snp.bottom : calendarView.snp.bottom).offset(1)
+            make.leading.equalToSuperview()
+        }
+        
+        cancleButton.snp.makeConstraints { make in
+            make.top.equalTo(messageLabel.snp.bottom).offset(90)
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview().inset(16)
+            make.height.equalTo(40)
+        }
+        
+        submitButton.snp.makeConstraints { make in
+            make.top.equalTo(messageLabel.snp.bottom).offset(90)
+            make.leading.equalTo(cancleButton.snp.trailing).offset(8)
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(16)
+            make.width.equalTo(cancleButton.snp.width)
+            make.height.equalTo(40)
         }
     }
 }
