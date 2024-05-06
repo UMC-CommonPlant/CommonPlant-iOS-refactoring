0920b9325b69331ece006ce648670493d8fdc68e
SunAra25 <tjsdkfk77@naver.com>
2024-05-06 19:38:06 +0900

    [REMOVE] 필요없는 주석 제거 #32

--- CommonPlant/CommonPlant/Resource/Supports/SceneDelegate.swift
+++ CommonPlant/CommonPlant/Resource/Supports/SceneDelegate.swift
@@ -15,9 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        //guard let _ = (scene as? UIWindowScene) else { return }
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        // 뷰 확인하기
+        
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
