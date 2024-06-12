51b345d23c34207d389d08ce6b4c9008f39a300b
SunAra25 <tjsdkfk77@naver.com>
2024-06-12 20:27:01 +0900

    [FEAT] MyPlantViewModel Input/Output 구조 적용 #35

--- CommonPlant/CommonPlant/Screen/MyPlant/ViewModel/MyPlantViewModel.swift
+++ CommonPlant/CommonPlant/Screen/MyPlant/ViewModel/MyPlantViewModel.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 class MyPlantViewModel {
     var myPlant: MyPlant
+    let plantIdx: Int
     
-    init() {
+    let disposeBag = DisposeBag()
+    
+    init(_ plantIdx: Int) {
+        self.plantIdx = plantIdx
         let plant = MyPlant(
             nickname: "몬테", scientificName: "Monstera deliciosa", place: "스윗홈_거실",
             imgURL: "https://commonplantbucket.s3.ap-northeast-2.amazonaws.com/f2e8f083-56aa-4952-8f1e-d07160c21036..jpg",
@@ -22,5 +27,63 @@ class MyPlantViewModel {
             ])
         
         myPlant = plant
+    }
+}
+
+extension MyPlantViewModel: ViewModelType {
+    struct Input {
+        let menuBtnDidTap: Observable<Void>
+        let editBtnDidTap: Observable<Void>
+        let deleteBtnDidTap: Observable<Void>
+        let alertDeleteBtnDidTap: Observable<Void>
+        let alertCancelBtnDidTap: Observable<Void>
+        let backgroundViewDidTap: Observable<Void>
+        let writeBtnDidTap: Observable<Void>
+        let memoListDidTap: Observable<Void>
+    }
+    
+    struct Output {
+        let showMenu: Driver<Void>
+        let showEditView: Driver<Int>
+        let showDeleteAlert: Driver<Void>
+        let backgroundHidden: Driver<Void>
+        let showAddMemoView: Driver<Int>
+        let showMemmoView: Driver<Int>
+        let popToPreviousView: Driver<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let showMenu = input.menuBtnDidTap.asDriver(onErrorDriveWith: .empty())
+        let showEditView = input.editBtnDidTap.map { [weak self] _ in
+            guard let self else { return 0 }
+            return plantIdx
+        }.asDriver(onErrorJustReturn: 0)
+        let showAddMemoView = input.writeBtnDidTap.map { [weak self] _ in
+            guard let self else { return 0 }
+            return plantIdx
+        }.asDriver(onErrorJustReturn: 0)
+        let showMemoView = input.memoListDidTap.map { [weak self] _ in
+            guard let self else { return 0 }
+            return plantIdx
+        }.asDriver(onErrorJustReturn: 0)
+        
+        let showDeleteAlert = input.deleteBtnDidTap.asDriver(onErrorDriveWith: .empty())
+        
+        let backgroundHidden = PublishSubject<Void>()
+        
+        input.backgroundViewDidTap.bind(to: backgroundHidden)
+            .disposed(by: disposeBag)
+        
+        input.alertCancelBtnDidTap.bind(to: backgroundHidden)
+            .disposed(by: disposeBag)
+        
+        let popToPreviousView = PublishSubject<Void>()
+        input.alertDeleteBtnDidTap.bind { _ in
+            // TODO: 식물 삭제 API
+            popToPreviousView.onNext(())
+        }.disposed(by: disposeBag)
+        
+        
+        return Output(showMenu: showMenu, showEditView: showEditView, showDeleteAlert: showDeleteAlert, backgroundHidden: backgroundHidden.asDriver(onErrorDriveWith: .empty()), showAddMemoView: showAddMemoView, showMemmoView: showMemoView, popToPreviousView: popToPreviousView.asDriver(onErrorJustReturn: ()))
     }
 }
