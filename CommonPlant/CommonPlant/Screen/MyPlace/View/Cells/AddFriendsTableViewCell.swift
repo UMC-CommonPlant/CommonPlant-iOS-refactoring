8fa2401796175671f97de8d7c003534bbc9b35f2
Yaewon Lee <yw990324@gmail.com>
2024-06-13 15:19:26 +0900

    [FEAT] 접근 제어자 수정 및 테이블뷰 셀 선택시 배경 색상 지정 #2
    configure() 추가

--- CommonPlant/CommonPlant/Screen/MyPlace/View/Cells/AddFriendsTableViewCell.swift
+++ CommonPlant/CommonPlant/Screen/MyPlace/View/Cells/AddFriendsTableViewCell.swift
@@ -13,40 +13,30 @@ import RxSwift
 
 class AddFriendsTableViewCell: UITableViewCell {
     static let identifier = "AddFriendsTableViewCell"
-    private let disposeBag = DisposeBag()
+    let disposeBag = DisposeBag()
     
     private let profileImageView = UIImageView().then {
         $0.image = UIImage(named: "ProfileGray")
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 20
     }
-    private let nameLabel = UILabel().then {
+    let nameLabel = UILabel().then {
         $0.font = .bodyM2
         $0.textColor = .gray6
         $0.text = "이름"
     }
-    private let checkImage = UIButton().then {
+    let checkImage = UIButton().then {
         $0.setImage(UIImage(named: "CheckEmpty"), for: .normal)
         $0.setImage(UIImage(named: "SelectedGreen"), for: .selected)
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupBindings()
         setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupBindings() {
-        checkImage.rx.tap
-            .bind { [weak self] in
-                guard let self = self else { return }
-                self.checkImage.isSelected.toggle()
-            }
-            .disposed(by: disposeBag)
     }
     
     private func setConstraints() {
@@ -72,5 +62,19 @@ class AddFriendsTableViewCell: UITableViewCell {
             make.centerY.equalToSuperview()
             make.right.equalToSuperview().offset(-20)
         }
+    }
+    
+    func configure(with friend: String) {
+        nameLabel.text = friend
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        contentView.backgroundColor = .white
+    }
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        contentView.backgroundColor = .white
     }
 }
